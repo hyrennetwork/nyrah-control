22f9833cfc63ca99fffa052d47fdefb7d05b321c
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-06-30 09:03:55 -0300

    pro saint ver

--- src/main/kotlin/net/hyren/nyrah/control/misc/vertx/buffer/Buffer.kt
+++ src/main/kotlin/net/hyren/nyrah/control/misc/vertx/buffer/Buffer.kt
@@ -11,23 +11,23 @@ fun Buffer.readVarInt(): Int {
     var output = 0
     var current = -1
 
-    do {
+    while (length() > 0) {
         if (++current > 2) {
             throw RuntimeException("Packet size is wider than 21 bits")
         }
 
         val byte = byteBuf.getByte(current)
 
-        output = output or (byte and 0x7F) shl current * 7
+        output = output or ((byte and 0x7F) shl current * 7)
 
-        if ((byte and 0x80) != 0x00) {
-            continue
+        if ((byte and 0x80) == 0) {
+            return output
         }
 
         if (output == 0x00) {
             throw CorruptedFrameException("Packet size cannot be 0")
         }
-    } while (length() == 0x00)
+    }
 
-    return output
+    return -1
 }
